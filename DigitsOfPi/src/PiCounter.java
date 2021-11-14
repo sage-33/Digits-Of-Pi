d0c5e2f7cc775dfbf0945245eef009718a4b7028
Sage Silberman <ssilberman2023@chadwickschool.org>
2021-11-14 01:23:54 -0800

    feat: Add Comments

--- DigitsOfPi/src/PiCounter.java
+++ DigitsOfPi/src/PiCounter.java
@@ -1,10 +1,28 @@
 import java.io.IOException;
 
+/**
+ * Represents a file consisting of all digits of pi where each character is
+ * scanned. Based on the character, the score for a digit or bad symbol is
+ * increased until there are no more characters, resulting with the total number
+ * of each digit from 0 to 9 and the total number of non digit (bad) symbols
+ * being printed out
+ *
+ * @author sagesilberman
+ *
+ */
 public class PiCounter extends TextFileAccessor {
-	private final int[] NUMBERS = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-	private final int ASCII_CODE_0 = 48;
-	private int badScore;
+	private int[] scores = new int[10]; // scores array
+	private final int ASCII_CODE_0 = 48; // a number that represents the number "0"
+	private int badScore; // holds the number of non-numbers present
 
+	/**
+	 * Initializes the code by setting score as an array with ten values and
+	 * badScore with a starting value of 0. Opens the file but throws IOException if
+	 * it can't open the file
+	 * 
+	 * @param filename
+	 * @throws IOException if file can't be opened
+	 */
 	public PiCounter(String filename) throws IOException {
 		openFile(filename);
 
@@ -12,39 +30,24 @@ public class PiCounter extends TextFileAccessor {
 
 	@Override
 	protected void processLine(String curLine) {
+		char[] ch = curLine.toCharArray();
 		for (int i = 0; i < curLine.length(); i++) {
-			// char ch =
-			// is this right?
-			if (Character.isDigit(ASCII_CODE_0) && getLetterPos(ch) < 26) {
-				int letterScore = (SCRABBLE_SCORES[getLetterPos(ch)]);
-
-				if (i % 4 == 0) {
-//					letterScore *= 2; Same thing as below but different format
-					letterScore = letterScore * 2;
-
-				} else if (i % 9 == 0) {
-
-					letterScore = letterScore * 3;
-
-				}
-
-				curLineScore = curLineScore + letterScore;
+			if (Character.isDigit(ch[i])) {
+				scores[ch[i] - ASCII_CODE_0]++;
 			}
 
+			else {
+				badScore++;
+			}
 		}
-		if (highestScore < curLineScore) {
-			highestScore = curLineScore;
-			winningString = curLine;
-		}
-	}
-
-	private int getLetterPos(char ch) {
-		return ch - ASCII_CODE_a;
 	}
 
 	@Override
 	public void printReport() {
-		System.out.println("winner: \"" + winningString + "\" score: " + highestScore);
+		for (int i = 0; i < scores.length; i++) {
+			System.out.println(i + " " + scores[i]);
+		}
+		System.out.println("bad symbols " + badScore);
 
 	}
 
--- DigitsOfPi/src/PiDriver.java
+++ DigitsOfPi/src/PiDriver.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
+/**
+ * If given a file, run Pi Driver and calculate the distributions of numbers in
+ * said file and how many non numbers there are.
+ * 
+ * @author sagesilberman
+ *
+ */
 public class PiDriver {
 
 	public static void main(String args[]) {
 		try {
-			Scanner scanner = new Scanner(System.in);
-			System.out.println("enter file name");
-			String filename = scanner.next();
-			PiCounter scrabble = new PiCounter(filename);
-			scrabble.processFile();
-			scrabble.printReport();
-			scanner.close();
+			PiCounter e = new PiCounter("corruptedDigitsOfPi.txt");
+			e.processFile();
+			e.printReport();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
